8e70f0267afc52e0933eff6e55db5cc335cdbdc2
jayce <jaycethanks@outlook.com>
2022-09-02 23:18:51 +0800

    可爱的眼睛2022年 09月 02日 星期五 23:18:51 CST

--- demos/scale-blur/scale-blur/script.js
+++ demos/scale-blur/scale-blur/script.js
@@ -3,13 +3,12 @@ const wrapper = document.querySelector("#wrapper");
 let basic = 240;
 let blur = 16;
 document.addEventListener("wheel", (event) => {
-  console.log(bg.style.width,"---",wrapper.style.backdropFilter)
   
-  basic += Number((event.deltaY * 0.5).toFixed(0));
+  basic += Number((event.deltaY * 5).toFixed(0));
   bg.style.width = `${basic}px`;
   bg.style.height = `${basic}px`;
   
-  blur += Number((event.deltaY * -0.01).toFixed(4));
+  blur += Number((event.deltaY * -0.1).toFixed(4));
   wrapper.style.backdropFilter=`blur(${blur}px)`;
 
   
