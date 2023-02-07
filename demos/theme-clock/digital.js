24d7669f49f3b105862f46a08ac826b1a81c309a
jayce <jaycethanks@outlook.com>
2023-02-07 09:45:33 +0800

    -

--- demos/theme-clock/digital.js
+++ demos/theme-clock/digital.js
@@ -8,7 +8,6 @@ flips.forEach((flip) => {
   flip.classList.add("down");
 });
 setInterval(update, 1000);
-let isFlipping = false;
 function update() {
   const time = new Date().toTimeString().split(" ")[0].split(":").join("").split("");
   flips.forEach((flip, index) => {
@@ -17,20 +16,19 @@ function update() {
       if (e) {
         const M = digital.className.match(/number\d/);
         // 数字一样则不用更新
-        // if (!isFlipping) {
         if (M[0] !== `number${time[index]}`) {
-          digital.className = digital.className.replace(/number\d/, `number${time[index]}`);
+          flip.children[1].className = flip.children[1].className.replace(/number\d/, `number${time[index]}`);
+          // digital.className = digital.className.replace(/number\d/, `number${time[index]}`);
           flip.classList.add("go");
           flip.classList.add("down");
-          // isFlipping = true;
+          isFlipping = true;
           setTimeout(() => {
             flip.classList.remove("go");
             flip.classList.remove("down");
-            // isFlipping = false;
+            flip.children[0].className = flip.children[0].className.replace(/number\d/, `number${time[index]}`);
             // 动画完成以后就可以移除动画类名 所以在 600 < x < 1000 以内都是可以的
           }, 650);
         }
-        // }
       } else {
         digital.classList.add(`number${time[index]}`);
       }
