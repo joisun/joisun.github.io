4bac56841154ac487e9ff44685e5723f6dddc1d8
jayce <jaycethanks@outlook.com>
2023-02-14 10:36:10 +0800

    -

--- demos/rabbitChat/server/index.js
+++ demos/rabbitChat/server/index.js
@@ -1,14 +1,8 @@
 import { WebSocketServer } from 'ws'
-
 const wss = new WebSocketServer({ port: 8080 })
-
 wss.on(
   'connection',
   function connection(ws, req) {
-    // const ip = req.socket.remoteAddress;
-    // wss.clients.forEach((it) => {
-    //   it.send('');
-    // });
     ws.on('message', function message(data) {
       console.log('[data]: ', data.toString())
       wss.clients.forEach((it) => {
