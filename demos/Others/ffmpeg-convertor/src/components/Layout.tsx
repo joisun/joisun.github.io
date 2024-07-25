96fdb635690edc10d17271deb3c06789c861e22f
jaycethanks <jaycethanks@outlook.com>
2024-07-25 17:07:36 +0800

    chore: basic theme

--- demos/Others/ffmpeg-convertor/src/components/Layout.tsx
+++ demos/Others/ffmpeg-convertor/src/components/Layout.tsx
@@ -20,7 +20,7 @@ interface SettingsLayoutProps {
 export default function Layout() {
   return (
     <>
-      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <div className="flex  flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
           <SidebarNav items={sidebarNavItems} />
         </aside>
--- demos/Others/ffmpeg-convertor/src/lib/utils.ts
+++ demos/Others/ffmpeg-convertor/src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const cn = function(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
