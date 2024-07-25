4eee5bf0cb8cf2a8f978fd5d005f42b02b4954ba
jaycethanks <jaycethanks@outlook.com>
2024-07-25 12:40:16 +0800

    chore: init

--- demos/DemoPages/view-transition-demo/src/App.vue
+++ demos/DemoPages/view-transition-demo/src/App.vue
@@ -1,10 +1,76 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import {  RouterView } from 'vue-router'
+const isDark = ref(false)
+
+const handleThemeSwitch = ()=>{
+  isDark.value = !isDark.value
+  document.documentElement.classList.toggle('dark') 
+}
+
+
+
+function handleClick(e) {
+  // Fallback for browsers that don’t support this API:
+  if (!document.startViewTransition) {
+    handleThemeSwitch()
+    return;
+  }
+
+  // Get the click position, or fallback to the middle of the screen
+  const x = e?.clientX ?? innerWidth / 2;
+  const y = e?.clientY ?? innerHeight / 2;
+  // Get the distance to the furthest corner
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  );
+
+  // Create a transition:
+  const transition = document.startViewTransition(() => {
+    console.log("trigger")
+    handleThemeSwitch()
+
+  });
+
+  // Wait for the pseudo-elements to be created:
+  transition.ready.then(() => {
+    // Animate the root’s new view
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0 at ${x}px ${y}px)`,
+          `circle(${endRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 500,
+        direction: isDark.value ? 'reverse' :'normal',
+        easing: "ease-in",
+        // Specify which pseudo-element to animate
+        pseudoElement: "::view-transition-new(root)",
+      },
+    );
+  });
+}
+
+
+
+
+
 </script>
 
 <template>
-  <div class="container px-8 py-2 font-sans  mx-auto">
-    <RouterView />
+  <div class="app-container  dark:bg-gray-900" >
+    <header class="text-right container mx-auto  px-12">
+      <button @click="handleClick" class="py-2 px-4 border 
+      dark:bg-white dark:hover:bg-white/70 dark:active:bg-white/60 dark:text-black
+      bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-white mt-2">Change {{ isDark }}</button>
+    </header>
+    <div class="container px-8 py-2 font-sans  mx-auto ">
+      <RouterView />
+    </div>
   </div>
 
 </template>
--- demos/DemoPages/view-transition-demo/src/views/HomeView.vue
+++ demos/DemoPages/view-transition-demo/src/views/HomeView.vue
@@ -14,19 +14,73 @@ const randomName = (l: number = 12) => {
   return _r.join('')
 }
 
-const handleClick = (i:number)=>{
-    router.push(`/detail/${i + 1}`)
+const jump = (i:number)=>{
+  router.push(`/detail/${i + 1}`)
 }
+
+
+function spaNavigate(data:number,e) {
+  // Fallback for browsers that don’t support this API:
+  if (!document.startViewTransition) {
+    jump(data)
+    return;
+  }
+
+  // Get the click position, or fallback to the middle of the screen
+  console.log( e?.clientX)
+  const x = e?.clientX ?? innerWidth / 2;
+  const y = e?.clientY ?? innerHeight / 2;
+  // Get the distance to the furthest corner
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  );
+
+  // Create a transition:
+  const transition = document.startViewTransition(() => {
+    jump(data)
+
+  });
+
+  // Wait for the pseudo-elements to be created:
+  transition.ready.then(() => {
+    // Animate the root’s new view
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0 at ${x}px ${y}px)`,
+          `circle(${endRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 500,
+        easing: "ease-in",
+        // Specify which pseudo-element to animate
+        pseudoElement: "::view-transition-new(root)",
+      },
+    );
+  });
+}
+
+
+
+const handleClick = (i:number,e)=>{
+  // document.startViewTransition(() => {
+    // router.push(`/detail/${i + 1}`)
+    spaNavigate(i,e)
+  // })
+}
+
 </script>
 
 <template>
   <main>
-    <h1 class="font-bold text-3xl">Easybuy</h1>
-      <h2 class="text-xl mt-2 text-gray-400">Rerum officia hic qui ipsam eveniet non reiciendis.</h2>
+    <h1 class="font-bold text-3xl dark:text-white">Easybuy</h1>
+      <h2 class="text-xl mt-2 text-gray-400 dark:text-white/80">Rerum officia hic qui ipsam eveniet non reiciendis.</h2>
       <div class="item-list mt-12 grid grid-cols-4 gap-y-4 gap-x-2">
-        <div class="item " v-for="(image, index) in 16" :key="index" @click="handleClick(index)">
+        <div class="item " v-for="(image, index) in 16" :key="index" @click="e=>handleClick(index,e)">
             <img class="aspect-square w-full  cursor-pointer hover:border-gray-200 border border-transparent p-1" :src="`https://placedog.net/300/300?id=${index + 1}`" :alt="`Image ${index + 1}`">
-          <p class="name font-semibold"> {{ randomName(8) }}</p>
+          <p class="name font-semibold dark:text-gray-200"> {{ randomName(8) }}</p>
           <p class="text-gray-500">{{ randomName(12) }}.</p>
           <p class="text-right font-semibold text-gray-700 text-sm">$99.00</p>
         </div>
