0846b0d29d2291ff4d57609185339e1bccb0d0cd
jaycethanks <jaycethanks@outlook.com>
2024-07-26 21:51:52 +0800

    -

--- demos/Others/ffmpeg-convertor/vite.config.ts
+++ demos/Others/ffmpeg-convertor/vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'./',
-  resolve:{
-    alias:{
-      '@':fileURLToPath(new URL('./src', import.meta.url))
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server:{
+  server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
